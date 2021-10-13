04a00a31b867086ccaac269da89f6c33c4cf6c6e
Duncan Goodare <55392744+dgoodare@users.noreply.github.com>
2021-10-13 15:00:28 +0100

    added functions for to multi-client connection
    not fully functional yet

--- server.py
+++ server.py
@@ -1,33 +1,79 @@
 import socket
+import selectors
+import types
 
-#set up localhost IP address
-hostAddress = '127.0.0.1'
-#set up a non-privileged port for the server to listen on (>1023)
-port = 500
+class Server:
 
-#set up a listening socket
-#socket() -> bind() -> listen() -> accept()
-def createServerSocket(hostAddress, port):
-    
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sckt:
-        sckt.bind((hostAddress, port))#bind the socket to localHost address and non-privileged port
-        sckt.listen()#get socket to listen
+    def __init__(self, host, port):
+        #set up localhost IP address
+        self.hostAddress = host
+        #set up a non-privileged port for the server to listen on (>1023)
+        self.port = port
+        #create a selector object to handle multiple client connections
+        self.slct = selectors.DefaultSelector()
+        self.sckt = self.createSocket()
 
-        #For now, the socket has been setup using IPv4, but we may need to change this to IPv6.
-        #Doing so will require the addition of flowinfo and scopeid
-        clientConnection, clientAddress = sckt.accept()
-        with clientConnection:
-            print(clientAddress, " connected to the server")
-            while True:#loop through blocking calls and read data sent by client
-                clientData = clientConnection.recv(1024)
+    def createSocket(self):
+        #create a socket object
+        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sckt.bind((self.hostAddress, self.port))
+        sckt.listen()
+        print("Listening on:", (self.hostAddress, self.port))
 
-                #if clientData is empty, then client closed connection and the loop is ended
-                if not clientData:
-                    break#end loop
-                
-                clientConnection.sendall(clientData)#echoes back the data sent by client
+        #set the scoket to non-blocking mode
+        sckt.setblocking(False)
+        #register the socket to be monitored for Read Events
+        self.slct.register(sckt, selectors.EVENT_READ, data=None)
+
+        return sckt
+
+    def startEventLoop(self):
+        while True:
+            #block until sockets are ready
+            events = self.slct.select(timeout=None)
+
+            for key, mask in events:
+                if key.data is None:
+                    self.getNewSocket(key.fileobj)
+                else:
+                    self.handleClientConnection(key, mask)
+
+    def getNewSocket(self, socket):
+        clientConn, clientAddr = socket.accept()
+        print(clientAddr, "has connected")
+        #put socket in non-blocking mode, otherwise, if it did block the entire server would stall
+        clientConn.setblocking(False)
+        #create object to hold client's data
+        clientData = types.SimpleNamespace(clientAddr=clientAddr, inb=b'', outb=b'')
+        #the client has data needing to be written/read, so both must be set
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+
+        self.slct.register(clientConn, events, data=clientData)
+
+    def handleClientConnection(self, key, mask):
+        socket = key.fileobj
+        clientData = key.data
+
+        #if there is a read event
+        if mask and selectors.EVENT_READ:
+            recvData = socket.recv(1024)
+            #if there is data to read
+            if recvData:
+                clientData.outb += recvData
+            else:
+                print(clientData.clientAddr, "has closed the connection")
+                self.slct.unregister(socket)
+                socket.close()
+
+        if mask and selectors.EVENT_WRITE:
+            if clientData.outb:
+                print('Echoing', repr(clientData.outb), 'to', clientData.clientAddr)
+                sentData = socket.send(clientData.outb)
+                clientData.outb = clientData[:sentData]
 
 
 
 
-createServerSocket(hostAddress, port)
+
+newServer = Server('127.0.0.1', 500)
+
