47805076cddbe03063e853d711b280b3252b480c
Duncan Goodare <55392744+dgoodare@users.noreply.github.com>
2021-10-17 14:52:53 +0100

    fixed issues with blocking
    had to retrace some steps and rebuild some aspects i.e Client and Server are no longer defined as classes

--- client.py
+++ client.py
@@ -1,112 +1,82 @@
+import sys
 import socket
 import selectors
 import types
 
 slct = selectors.DefaultSelector()
+messages = [b"Message 1", b"Message 2"]
 
-class Client:
+def receiveArgs():
+    #check if number of arguments is correct
+    if len(sys.argv) != 4:
+        print("Incorrect number of arguments:", sys.argv[0], "<host address> <port number> <number of connections>")
+        sys.exit(1)
+    
+    hostAddr, portNo, connections = sys.argv[1:4]
+    beginConnection(hostAddr, int(portNo), int(connections))
 
-    def __init__(self, host, port):
-        self.hostAddress = host
-        self.port = port
-        self.messages = [b'Message 1', b'Message 2']
-        #self.slct = selectors.DefaultSelector()
 
-    def beginConnection(self, numOfConnections):
-        serverAddr = (self.hostAddress, self.port)
+def beginConnection(host, port, connections):
+    serverAddr = (host, port)
+    #iterate through the different connections, creating a socket for each
+    for i in range(0, connections):
+        connid = i + 1
+        print("starting connection", connid, "to", serverAddr)
+        #create socket object,
+        #currently only IPv4, changin gto IPv6 will require the addition of 'flowinfo' and 'scopeID' in the tuple
+        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #set to non-blocking mode to prevent the server from stalling, 
+        #preventing other sockets from being serviced
+        sckt.setblocking(False)
+        #connect to server
+        sckt.connect_ex(serverAddr)
 
-        for i in range(0, numOfConnections):
-            connectionID = i+1
-            print("Connecting ", connectionID, "to", serverAddr)
-            #create new socket
-            sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #set socket to non-blocking mode
-            sckt.setblocking(False)
-            #begin connection to server
-            sckt.connect_ex(serverAddr)
+        #prepare an object to store the messages being sent to the server
+        clientData = types.SimpleNamespace(
+            connID=connid,
+            msgSize=sum(len(m) for m in messages),
+            recvTotal=0,
+            messages=list(messages),
+            outb=b"",
+        )
+        #create object to monitor for read/write events
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        #register socket with the selector
+        slct.register(sckt, events, data=clientData)
 
-            #set up socket for reading and writing
-            events = selectors.EVENT_READ | selectors.EVENT_WRITE
 
-            clientData =  types.SimpleNamespace(connectionID=connectionID, 
-                                                msgSize = sum(len(x) for x in self.messages),#number of bytes
-                                                recvTotal = 0,#size of data that has been received
-                                                messages = list(self.messages),#the messages themselves, stored as a list
-                                                outb = b'')
-            slct.register(sckt, events, data=clientData)
+def serviceConnection(key, mask):
+    sckt = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recvData = sckt.recv(1024)
+        if recvData:
+            print("received", repr(recvData), "from connection", data.connID)
+            data.recvTotal += len(recvData)
+        if not recvData or data.recvTotal == data.msgSize:
+            print("closing connection", data.connID)
+            slct.unregister(sckt)
+            sckt.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print("sending", repr(data.outb), "to connection", data.connID)
+            sent = sckt.send(data.outb)
+            data.outb = data.outb[sent:]
 
-    def tryConnect(self):
-        try:
-            while True:
-                events = slct.select(timeout=0)
-                if events:
-                    for key, mask in events:
-                        self.handleConnection(key, mask)
 
-                if not slct.get_map():
-                    break
-        
-        except KeyboardInterrupt:
-            print("Keyboard input detected, closing connection")
-        finally:
-            slct.close()
+receiveArgs()
 
-    def handleConnection(self, key, mask):
-        socket = key.fileobj
-        clientData = key.data
-
-        #if there is a read event
-        if mask and selectors.EVENT_READ:
-            recvData = socket.recv(1024)
-            #if there is data to read
-            if recvData:
-                print("Received", repr(recvData), "from", clientData.connectionID)
-                #increment running total of data received
-                clientData.recvTotal += len(recvData)
-            
-            #check to see if the size of the message matches the size of the data received
-            #It is possible that the client doesn't close its connection - causing the server to leave
-            #the connection open. This is something we may want to guard against on the serverside, 
-            #otherwise, unused client connections may end up accumulating unnecessarily
-            if (not recvData) or (clientData.recvTotal == clientData.msgSize):
-                #if data has finished transmitting (i.e. recvTotal == msgSize) close the connection
-                print("closing connection:", clientData.connectionID)
-                slct.unregister(socket)
-                socket.close()
-
-        #if there is a write event
-        if mask and selectors.EVENT_WRITE:
-            if not clientData.outb and clientData.messages:
-                #get the first message from the list
-                clientData.outb = clientData.messages.pop(0)
-            #if outb contains a message
-            if clientData.outb:
-                print("sending", repr(clientData.outb), "to connection", clientData.connectionID)
-                sentData = socket.send(clientData.outb)
-                clientData.outb = clientData.outb[sentData:]
-
-newClient = Client('127.0.0.1', 500)
-newClient.beginConnection(1)
-#newClient.tryConnect()
 try:
     while True:
         events = slct.select(timeout=1)
         if events:
             for key, mask in events:
-                newClient.handleConnection(key, mask)
+                serviceConnection(key, mask)
         if not slct.get_map():
             break
-
 except KeyboardInterrupt:
-    print("Keyboard input detected, closing connection")
+    print("caught keyboard interrupt, exiting")
 finally:
     slct.close()
-
-            
-
-
-
-
-
-
-
--- server.py
+++ server.py
@@ -1,80 +1,78 @@
+import sys
 import socket
 import selectors
 import types
 
-class Server:
+slct = selectors.DefaultSelector()
 
-    def __init__(self, host, port):
-        #set up localhost IP address
-        self.hostAddress = host
-        #set up a non-privileged port for the server to listen on (>1023)
-        self.port = port
-        #create a selector object to handle multiple client connections
-        self.slct = selectors.DefaultSelector()
-        self.sckt = self.createSocket()
+def startServer():
+    #check for correct number of arguments
+    if len(sys.argv) != 3:
+        print("usage:", sys.argv[0], "<host> <port>")
+        sys.exit(1)
 
-    def createSocket(self):
-        #create a socket object
-        sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sckt.bind((self.hostAddress, self.port))
-        sckt.listen()
-        print("Listening on:", (self.hostAddress, self.port))
+    hostAddr, portNo = sys.argv[1], int(sys.argv[2])
+    #create socket object, bind to given address and port number
+    sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sckt.bind((hostAddr, portNo))
+    sckt.listen()
+    print("listening on", (hostAddr, portNo))
+    #set to non-blocking mode
+    sckt.setblocking(False)
+    #register with selector
+    slct.register(sckt, selectors.EVENT_READ, data=None)
 
-        #set the scoket to non-blocking mode
-        sckt.setblocking(False)
-        #register the socket to be monitored for Read Events
-        self.slct.register(sckt, selectors.EVENT_READ, data=None)
-
-        return sckt
-
-    def startEventLoop(self):
+def startEventLoop():
+    try:
         while True:
-            #block until sockets are ready
-            events = self.slct.select(timeout=None)
-
+            events = slct.select(timeout=None)
             for key, mask in events:
                 if key.data is None:
-                    self.getNewSocket(key.fileobj)
+                    acceptWrapper(key.fileobj)
                 else:
-                    self.handleClientConnection(key, mask)
+                    serviceConnection(key, mask)
+    except KeyboardInterrupt:
+        print("caught keyboard interrupt, exiting")
+    finally:
+        slct.close()
 
-    def getNewSocket(self, socket):
-        clientConn, clientAddr = socket.accept()
-        print(clientAddr, "has connected")
-        #put socket in non-blocking mode, otherwise, if it did block the entire server would stall
-        clientConn.setblocking(False)
-        #create object to hold client's data
-        clientData = types.SimpleNamespace(clientAddr=clientAddr, inb=b'', outb=b'')
-        #the client has data needing to be written/read, so both must be set
-        events = selectors.EVENT_READ | selectors.EVENT_WRITE
-
-        self.slct.register(clientConn, events, data=clientData)
-
-    def handleClientConnection(self, key, mask):
-        socket = key.fileobj
-        clientData = key.data
-
-        #if there is a read event
-        if mask and selectors.EVENT_READ:
-            recvData = socket.recv(1024)
-            #if there is data to read
-            if recvData:
-                clientData.outb += recvData
-            else:
-                print(clientData.clientAddr, "has closed the connection")
-                self.slct.unregister(socket)
-                socket.close()
-
-        if mask and selectors.EVENT_WRITE:
-            if clientData.outb:
-                print('Echoing', repr(clientData.outb), 'to', clientData.clientAddr)
-                sentData = socket.send(clientData.outb)
-                clientData.outb = clientData[:sentData]
+def acceptWrapper(sckt):
+    conn, addr = sckt.accept()
+    print("Connection from", addr, "accepted")
+    #set socket to non-blocking mode
+    conn.setblocking(False)
+    #create data object to store client data
+    clientData = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    #create object to monitor for read/write events
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    #register with selector
+    slct.register(conn, events, data=clientData)
 
 
+def serviceConnection(key, mask):
+    sckt = key.fileobj
+    data = key.data
+
+    #if there is a read event
+    if mask & selectors.EVENT_READ:
+        recvData = sckt.recv(1024)
+        #if there is data to read
+        if recvData:
+            data.outb += recvData
+        #if there isn't, close the connection
+        else:
+            print("closing connection to", data.addr)
+            slct.unregister(sckt)
+            sckt.close()
+
+    #if there is a write event
+    if mask & selectors.EVENT_WRITE:
+        #if data exists, send
+        if data.outb:
+            print("echoing", repr(data.outb), "to", data.addr)
+            sent = sckt.send(data.outb)
+            data.outb = data.outb[sent:]
 
 
-
-newServer = Server('127.0.0.1', 500)
-newServer.startEventLoop()
-
+startServer()
+startEventLoop()
