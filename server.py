f880e76afdc9512de5afbc8de50fb8eff147c03d
Duncan Goodare <55392744+dgoodare@users.noreply.github.com>
2021-10-23 17:19:24 +0100

    added private messages

--- server.py
+++ server.py
@@ -12,10 +12,13 @@ serverSckt = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 serverSckt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serverSckt.bind((IPaddress, portNo, 0 ,0))
 serverSckt.listen()
+print("Listening on", (IPaddress, portNo))
 #create list to store all sockets, including the server socket
 socketList = [serverSckt]
-#create list of users
+#create list of clients
 clientList = {}
+#create list of usernames
+usernameList = []
 
 
 def recvMessage(clientSckt):
@@ -34,6 +37,13 @@ def recvMessage(clientSckt):
     except:
         return False
 
+def getUser(message):
+    #remove the '@' (the first character)
+    message = message[1:]
+    #split the message by spaces
+    user = message.split(' ')
+    #split() returns a tuple where each element is a word, we only need the first element
+    return user[0]
 
 while True:
     #read list, write list, error list
@@ -56,6 +66,7 @@ while True:
 
             clientIP, clientPort = clientAddr[0], clientAddr[1]
             clientName = newUser['data'].decode('utf-8')
+            usernameList.append(clientName)
             print("Connection from", clientIP, clientPort, "with username:", clientName, "accepted")
         else:
             message = recvMessage(notified)
@@ -68,11 +79,37 @@ while True:
                 continue
             
             user = clientList[notified]
-            print(f"Message received from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+            userMsg = message['data'].decode('utf-8')
+            print(f"Message received from {user['data'].decode('utf-8')}: {userMsg}")
 
-            for clientSckt in clientList:
-                if clientSckt != notified:
-                    clientSckt.send(user['header'] + user['data'] + message['header'] + message['data'])
+            #check for private message
+            if userMsg[0] == '@':
+                userMatch = False
+                #retrieve the username from the message
+                recipient = getUser(userMsg)
+                if recipient:
+                    #start counnter at 1, as the first item in socketList is the server socket
+                    counter = 1
+                    #iterate through the list of usernames and check for matches
+                    for username in usernameList:
+                         if recipient == username:
+                             userMatch = True
+                             break
+                         counter += 1
+                    
+                    if userMatch:
+                        #get the user's socket from the corresponding socketList
+                        clientSckt = socketList[counter]
+                        #send the message to only the specified user
+                        clientSckt.send(user['header'] + user['data'] + message['header'] + message['data'])
+                    else:
+                        #if no matching username could be found
+                        print("No user with the name", recipient, "could be found")
+            else:
+                #not a private message so send to all users
+                for clientSckt in clientList:
+                    if clientSckt != notified:
+                        clientSckt.send(user['header'] + user['data'] + message['header'] + message['data'])
 
     #iterate through the error list and remove any problematic sockets
     for notified in exceptionSckt:
